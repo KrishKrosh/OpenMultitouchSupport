da9157e79ab11d1b1137625cade1b9eb0459f35f
KrishKrosh <krish.life21@gmail.com>
2025-07-28 01:08:30 -0400

    Fix Package.swift syntax error - remove commented lines that were breaking SPM resolution

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenMultitouchSupportXCF",
-            // For development: use local framework
-            // path: "OpenMultitouchSupportXCF.xcframework"
-            // For release: use GitHub URL (replaced by release script)
             url: "https://github.com/KrishKrosh/OpenMultitouchSupport/releases/download/v1.0.11/OpenMultitouchSupportXCF.xcframework.zip",
             checksum: "12e302ec4c08a24c5a78be3ff67417191f0f010165325325553116837d2e7505"
         ),
