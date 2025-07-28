811121744fa6f7f55afe5e13532e1bb7cccf73ee
KrishKrosh <krish.life21@gmail.com>
2025-07-28 00:41:01 -0400

    updated package.swift

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenMultitouchSupportXCF",
+            url: "https://github.com/KrishKrosh/OpenMultitouchSupport/releases/download/v1.0.6/OpenMultitouchSupportXCF.xcframework.zip",
+            checksum: "175cb56a24ae0d085d7869aae332dd28bd43de5fe5a048f0f888a62c2a4f86c5",
             path: "OpenMultitouchSupportXCF.xcframework"
         ),
         .target(
